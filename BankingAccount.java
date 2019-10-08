46288357586339f6be779b745a54a6b48031f79e
Dhivya <dhivya@PrabhuanandsAir.hsd1.ca.comcast.net>
2019-10-07 22:50:20 -0700

    updating banking project

--- BankingAccount.java
+++ BankingAccount.java
@@ -2,6 +2,10 @@ package com.flexon.corejava;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class BankingAccount
 {
@@ -10,13 +14,19 @@ public class BankingAccount
 	static String name = "abc";
 	String email;
 	int mobilenum;
-	static float bal=10000;
+	static float  bal= 10000;
 	float nb;
-	
+	float limit_depo=0;
 	int i;
 	float depo,wd;
 	
-
+	
+	
+	private void balance()
+	{
+		System.out.println("your balance is : "+bal);
+	}
+	
 	public String details()
 	{
 		int number = 123456789;
@@ -28,43 +38,76 @@ public class BankingAccount
 		System.out.println( "Account number : "+number +"\nCustomername : "+name +"\nCustomer email : "+email+"\nMobile number : "+mobilenum);
 		return name;
 	}
+	public String showbalance()
+	{
+		String firstname="Dhivya";
+		String lastname="Sekaran";
+		String acc_type="savings";
+		float acc_bal= bal;
+		
+		
+		DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+	    Date dateobj = new Date();
+	    System.out.println("First name is "+firstname);
+	    System.out.println("Last name is "+lastname);
+	    System.out.println("Account type is "+acc_type);
+	    System.out.println("Account balance  is "+acc_bal);
+	    System.out.println(df.format(dateobj));
+		
+		
+		//Date date = new Date();
+		//long time = date.getTime();
+		
+		//Timestamp ts =new Timestamp(time);
+		
+		//System.out.println("time is"+ts);
+		
+		return firstname;
+	}
+	
 	
 	private float deposit(float depo)
 	{
 		
 		this.depo=depo;
-		nb =( depo + bal);
-		System.out.println("your new balance is : "+nb);
+		limit_depo=(limit_depo+depo);
+		if(limit_depo<=1500)
+		{
+			nb =( depo + bal);
+			bal = nb;
+			System.out.println("your new balance is : "+bal);
+		}
+		else
+			System.out.println("transaction limit exceeded");
 		return nb;
-	}
-	
-	float withdraw(float wd){
-	{
-		if(wd<=1000) {
-			System.out.println("withdrawal in process");
 		
+	} 
+	
+	float withdraw(float wd)
+	{
+		{
+		if(wd<=1000) 
+			{
+			System.out.println("withdrawal in process");					
 			this.wd=wd;
 			nb=(bal - wd);
-			System.out.println("your new balance is : "+nb);
-			
-		}
-		else {
-			System.out.println("withdrawal amount exceeds");
-			
-			
-	
-		}
+			bal = nb;
+			System.out.println("your new balance is : "+bal);			
+		
+			}
+			else 
+			{
+			System.out.println("withdrawal amount exceeds");	
+			}
 		   
-	}
+		}
 	 ///System.out.println("your account balance is "+bal);
-	return nb;	
+		return nb;	
 	
 	
 	}
-	private void balance()
-	{
-		System.out.println("your balance is : "+bal);
-	}
+	
+	
 	
 	
 	private void exit()
@@ -75,11 +118,13 @@ public class BankingAccount
 	
 	
 	
+	
 	public static void main(String args[])
 	{
 		BankingAccount acc =new BankingAccount();
 		UserDetails det=new UserDetails();
 			int i;
+			int x;
 			int ans;
 		float depo,wd;
 		DataInputStream b=new DataInputStream(System.in);
@@ -87,28 +132,60 @@ public class BankingAccount
 		det.UserDetails(name);
 		System.out.println(" Hi "+name);
 		
-		System.out.println("Please enter the number 1 to 5");
+		System.out.println("Please enter the number 1 to 6");
 		System.out.println("1. Details");
         System.out.println("2. Deposit");
         System.out.println("3. Withdraw");
         System.out.println("4. Balance");
         System.out.println("5. Logout");
+        System.out.println("6.show balnce");
+        
         System.out.print("Enter Selection: ");
-		try {
+		try 
+		{
 			i = Integer.parseInt(b.readLine());
 		
-	      switch(i)
-	      {
-		 case 1:
-			   System.out.println("Details: ");
-			   acc.details();
-			   break;
-		 case 2:
-			   System.out.println("deposit :");
-			   System.out.println("please enter the amount to deposit");
-				depo=Float.parseFloat(b.readLine());
-			   acc.deposit(depo);
-			 
+			switch(i)
+			{
+				case 1:
+				   System.out.println("Details: ");
+				   acc.details();
+				   break;
+				case 2:
+				   System.out.println("deposit :");
+				   System.out.println("please enter the amount to deposit");
+				   depo=Float.parseFloat(b.readLine());
+				   if(depo<=500 && depo >= 50) 
+				   {		
+					   acc.deposit(depo);
+					}
+					else 
+					{
+						System.out.println("amonut limit is 50 to 500");	
+					}
+					for(x=1;x<3;x++) 
+					{
+						System.out.println("do you want to continue withdraw..yes=1 or no=2");
+						ans = Integer.parseInt(b.readLine());
+						{
+							if(ans==1)
+						
+							{
+								System.out.println("please enter the amount to deposit");
+								depo=Float.parseFloat(b.readLine());
+								if(depo<=500 && depo >= 50) 
+								{
+							
+						
+										acc.deposit(depo);
+								}
+								else 
+								{
+									System.out.println("amonut limit is 50 to 500");	
+								}
+							}
+						}
+					}
 			   break;
 		 case 3:
 			 	System.out.println("withdraw : ");
@@ -120,26 +197,34 @@ public class BankingAccount
 				{
 					System.out.println("your balance low");
 				}
+				do {
 				System.out.println("do you want to continue withdraw..yes=1 or no=2");
 				ans = Integer.parseInt(b.readLine());
-				if (ans == 1 )
+				
+				
+				
+				if(ans==1)
 					
-				{
+				 {
 					System.out.println("please enter the amount to withdraw");
 					wd=Float.parseFloat(b.readLine());
-					if(wd<=bal) {
+						if(wd<=bal) {
 					 	acc.withdraw(wd);
-						}else
+						} 
+						else
 						{
 							System.out.println("your balance low");
 							
-						}	
-					
-					
-				}
-				else if (ans==2){
+						}
+				}}while(ans==1);
+				
+				
+				{
+				 if (ans==2){
 					System.out.println("exited");
 				}
+	      		}
+				
 			 	break;
 		 case 4:
 	           System.out.println("balance");
@@ -149,8 +234,12 @@ public class BankingAccount
 	           System.out.println("exit");
 	           acc.exit();
 	           break;
+		 case 6:
+			 	System.out.println("show balance");
+			 	acc.showbalance();
+			 	break;
 		 default:
-		   System.out.println("enter the number 1 to 5");
+		   System.out.println("enter the number 1 to 6");
 	      }
 	      
 	      
@@ -162,9 +251,11 @@ public class BankingAccount
 	    	   
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+				System.out.println("error occured");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+				System.out.println("error occured");
 			}
 		
 	}
